4ba53575e0047afe25e20e4f25a8cfdb8fc0e55d
reizergrzegorz <reizergrzegorz@gmail.com>
2025-03-23 14:10:22 +0100

    add index method to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,10 @@ class PostsController < ApplicationController
 
     before_action :authenticate_user!, except: :index
 
+    def index
+        @posts = Post.all
+    end
+
     def new 
         @post = Post.new
     end
