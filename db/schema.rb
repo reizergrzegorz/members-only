15667d7afc394b9154c710c8c8524cdc777f9952
reizergrzegorz <reizergrzegorz@gmail.com>
2025-03-23 14:17:18 +0100

    add association between user and post

--- db/schema.rb
+++ db/schema.rb
@@ -29,5 +29,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_23_124701) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  
+    add_foreign_key "posts", "users"
   end
 end
