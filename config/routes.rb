0893f3c6a26e70ad980ed6b4d13e8a263eb894b6
reizergrzegorz <reizergrzegorz@gmail.com>
2025-03-23 14:27:56 +0100

    add sign_out link and logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   
   root "posts#index"
+  
+  devise_scope :user do
+    get "users/sign_out" => "users/sessions#destroy"
+  end
+
   devise_for :users
   resources :posts, only: [:new, :create, :index]
 
