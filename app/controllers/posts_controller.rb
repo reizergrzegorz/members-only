f0b010b9b8f690353577bb901c08632d8fa458f8
reizergrzegorz <reizergrzegorz@gmail.com>
2025-03-23 14:07:49 +0100

    add create method to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,15 @@ class PostsController < ApplicationController
         @post = Post.new
     end
 
+    def create
+        @post = Post.new(post_params)
+        @post.user = current_user
+
+        if @post.save
+            redirect_to posts_path
+        else 
+            render :new, status: :unprocessable_entity
+        end
+    end
+
 end
